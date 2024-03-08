4ec81c6f3fd8c2de922051f8f05637c648fcd9f3
Landry Simo <86841267+Starland9@users.noreply.github.com>
2024-03-08 14:08:08 +0100

    Commit Description: - Refactors code for improved readability and efficiency - Removes unnecessary imports - Updates application initialization process for improved performance and readability - Adds functionality to update table data in PyQt - Connects button click to adding a row and updating the dataframe from the table

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 import sys
 import qdarktheme
 from PyQt6.QtWidgets import QApplication
 
-from widgets import home
+from src.widgets import home
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyleSheet(qdarktheme.load_stylesheet("dark"))
     window = home.Home()
--- src/widgets/home.py
+++ src/widgets/home.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QMainWindow, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QMessageBox, QTableWidgetItem
 from src.ui import home
 from src.widgets import about
+import pandas as pd
 
 
 class Home(QMainWindow, home.Ui_MainWindow):
@@ -13,9 +14,47 @@ class Home(QMainWindow, home.Ui_MainWindow):
 
         self.manage_actions()
 
+        self.table_data_frame = pd.DataFrame.from_dict(
+            {
+                "Modalités": [],
+                "Effectifs": [],
+                "Frequence": [],
+                "Centre": [],
+                "Repartition": [],
+                "Densité": [],
+            },
+        )
+
+        self.update_table_from_dataframe()
+
     def manage_actions(self):
         self.actionA_prop_os.triggered.connect(self.show_about)
+        self.btn_add.clicked.connect(self.add_row)
 
     def show_about(self):
         self.window_about = about.About()
         self.window_about.show()
+
+    def update_table_from_dataframe(self):
+        self.tab.setRowCount(len(self.table_data_frame))
+        self.tab.setColumnCount(len(self.table_data_frame.columns))
+        self.tab.setHorizontalHeaderLabels(self.table_data_frame.columns)
+        for i in range(len(self.table_data_frame)):
+            for j in range(len(self.table_data_frame.columns)):
+                self.tab.setItem(
+                    i, j, QTableWidgetItem(str(self.table_data_frame.iloc[i, j]))
+                )
+
+    def update_dataframe_from_table(self):
+        df_map = {}
+        for i in range(self.tab.rowCount()):
+            for j in range(self.tab.columnCount()):
+                if self.tab.item(i, j) is None:
+                    continue
+                df_map[self.tab.horizontalHeaderItem(j).text()] = self.tab.item(i, j).text()
+
+        print("df : ", self.table_data_frame.describe())
+
+    def add_row(self):
+        self.tab.setRowCount(self.tab.rowCount() + 1)
+        self.update_dataframe_from_table()
